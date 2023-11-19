24e26fdc575850dcf667c6e8cc269b5e534d42e1
salman3009 <salman444g@gmail.com>
2023-11-19 14:25:27 +0530

    Code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 //Write your code here
+class API{
+    #secure;
+    constructor(url,method='GET'){
+        this.url = url;
+        this.method = method;
+        this.#secure = url.startsWith('https');
+    }
 
+    isSecure(){
+        return this.#secure;
+    }
+
+    updateURL(url){
+     this.url = url;
+     this.#secure = url.startsWith('https');
+    }
+}
 
 
 
