be4484083c0aa19939a4a0066e51ea44924908fa
salman3009 <salman444g@gmail.com>
2023-11-19 14:31:16 +0530

    Code

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ class API{
     constructor(url,method='GET'){
         this.url = url;
         this.method = method;
-        this.#secure = url.startsWith('https');
+        this.#secure = url.startsWith("https");
     }
 
     isSecure(){
         return this.#secure;
     }
 
-    updateURL(url){
+     updateURL(url){
      this.url = url;
-     this.#secure = url.startsWith('https');
+     this.#secure = url.startsWith("https");
     }
 }
 
